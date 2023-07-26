52a4dffa24c5141f04183d96a89e7b9781d5a8b7
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2023-07-26 20:52:35 +0300

    create a redux structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-function App ()
-{
-    return <div>hi!</div>
+import CarForm from './components/CarForm';
+import CarList from './components/CarList';
+import CarSearch from './components/CarSearch';
+import CarValue from './components/CarValue';
+
+function App() {
+  return (
+    <div>
+      <CarForm />
+      <CarList />
+      <CarSearch />
+      <CarValue />
+    </div>
+  );
 }
 export default App;
