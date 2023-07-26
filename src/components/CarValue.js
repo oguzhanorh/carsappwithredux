9a79923d494a20982be6df38da35dbfa947c5d63
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2023-07-27 00:38:08 +0300

    calculate the car value

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,4 +1,18 @@
+import { useSelector } from 'react-redux';
+
 function CarValue() {
-  return <div>CarValue</div>;
+  const totalCost = useSelector(({ cars: { data, searchTerm } }) => {
+    const filteredCars = data.filter((car) => {
+      return car.name.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+    // let cost = 0;
+    // for (let car of filteredCars) {
+    //   cost += car.cost;
+    // }
+    return filteredCars.reduce((acc, car) => {
+      return acc + car.cost;
+    }, 0);
+  });
+  return <div className="car-value">Total Cost: ${totalCost}</div>;
 }
 export default CarValue;
